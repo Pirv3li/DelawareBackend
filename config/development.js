a0377d7553119e1e2e7617e948d3783247f81d20
Mohamed Alsagher <mohamed.alsagher@student.hogent.be>
2024-02-22 11:51:30 +0100

    databank connectie

--- config/development.js
+++ config/development.js
@@ -3,6 +3,20 @@ module.exports = {
     level: 'silly',
     disabled: false,
   },
+  cors: {
+    origins: ['http://localhost:5173'],
+    maxAge: 3 * 60 * 60, 
+  },
+
+  database: {
+    client: 'mysql2',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+
   auth: {
     argon: {
       saltLength: 16,
@@ -13,8 +27,9 @@ module.exports = {
     jwt: {
       secret: process.env.AUTH_JWT_SECRET,
       expirationInterval: 60 * 60 * 10000, 
-      issuer: 'backend',
-      audience: 'frontend',
+      issuer: 'http://localhost:9000',
+      audience: 'http://localhost:5173',
     },
   },
+  port: 10000,
 };
