b81465e606ac312046c299aa7cce7b4ac8b6b991
LucasHogent1 <lucas.katamadze@student.hogent.be>
2024-02-22 11:35:41 +0100

    migrations

---
+++ src/data/migrations/202401010000_createBedrijf.js
@@ -0,0 +1,27 @@
+const { tables } = require('..');
+
+module.exports = {
+  up: async (knex) => {
+    await knex.schema.createTable(tables.bedrijf, (table) => {
+      table.increments('idBedrijf').primary();
+      table.string('naam', 255).notNullable();
+      table.string('logo', 255).notNullable();
+      table.string('sector', 255).notNullable();
+      table.string('straat', 255).notNullable();
+      table.integer('nummer').notNullable();
+      table.integer('postcode').notNullable();
+      table.string('stad', 255).notNullable();
+      table.string('straat', 255).notNullable();
+      table.string('iban', 255).notNullable();
+      table.string('btwNumer', 255).notNullable();
+      table.string('telefoonnummer', 255).notNullable();
+      table.timestamp('gebruikerSinds').notNullable();
+      
+      
+
+    });
+  },
+  down: async (knex) => {
+    await knex.schema.dropTableIfExists(tables.bedrijf);
+  },
+};
---
+++ src/data/migrations/202401010012_createUser.js
@@ -0,0 +1,26 @@
+const { tables } = require('..');
+
+module.exports = {
+  up: async (knex) => {
+    await knex.schema.createTable(tables.user, (table) => {
+      table.increments('idUser').primary();
+      table.string('soort', 255).notNullable();
+
+      table.string('gebruikersnaam', 255).notNullable();
+      table.string('password_hash').notNullable(); 
+      table.boolean('isActief').notNullable();
+      table.jsonb('soort').notNullable();
+      table.string('klantnummer').notNullable();
+      table.string('leveranciernummer').notNullable();
+
+
+      table
+        .foreign('idBedrijf')
+        .references(`${tables.bedrijf}.idBedrijf`)
+        .onDelete('CASCADE');
+    });
+  },
+  down: async (knex) => {
+    await knex.schema.dropTableIfExists(tables.user);
+  },
+};
