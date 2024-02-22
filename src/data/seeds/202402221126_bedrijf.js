f606164c92a392386b4cb8dc17043adbdbbe8c10
Papuna <papuna.noniashvili@student.hogent.be>
2024-02-22 11:41:41 +0100

    foutje

--- src/data/seeds/202402221126_bedrijf.js
+++ src/data/seeds/202402221126_bedrijf.js
@@ -24,7 +24,7 @@ module.exports = {
         email: 'leverancier@hotmail.com',
         password_hash:
         '$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4',
-      roles: JSON.stringify([Role.LEVERANCIER]),
+      roles: JSON.stringify([Role.LEVER]),
       },
       {
         id: 3,
