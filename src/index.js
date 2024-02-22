72c2de499d1b03d0539fd7bb61484e0d6f3dfe02
Papuna <papuna.noniashvili@student.hogent.be>
2024-02-22 11:53:29 +0100

    createServer

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
-const Koa = require('koa');
-const config = require('config');
-const winston = require('winston');
+const createServer = require('./createServer'); 
 
-const NODE_ENV = config.get('env'); 
-const LOG_LEVEL = config.get('log.level'); 
-const LOG_DISABLED = config.get('log.disabled'); 
+async function main() { 
+ 
+  try {
+    const server = await createServer(); 
+    await server.start(); 
 
-console.log(`log level ${LOG_LEVEL}, logs enabled: ${LOG_DISABLED !== true}`);
+    async function onClose() {
+      await server.stop(); 
+      process.exit(0); 
+    }
 
-const app = new Koa();
-
-const logger = winston.createLogger({
-  level: LOG_LEVEL,
-  format: winston.format.simple(),
-  transports: [
-    new winston.transports.Console({ silent: LOG_DISABLED })
-  ]
-});
-
-app.use(async (ctx, next) => {
-  ctx.body = 'Hello world';
-});
-
-app.use(async (ctx, next) => {
-  logger.info(JSON.stringify(ctx.request));
-  return next();
-});
-
-app.listen(9000, () => {
-  logger.info('🚀 Server listening on http://localhost:9000');
-});
+    process.on('SIGTERM', onClose);
+    process.on('SIGQUIT', onClose);
+  } catch (error) {
+    console.error(error); 
+    process.exit(-1); 
+  }
+}
+main(); 
