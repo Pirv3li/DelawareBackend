2d9a6820fa504874a7790b3a1b7b050da528fe0c
LucasHogent1 <lucas.katamadze@student.hogent.be>
2024-02-22 12:44:43 +0100

    bedrijf seed

--- src/data/migrations/202401010012_createUser.js
+++ src/data/migrations/202401010012_createUser.js
@@ -2,22 +2,18 @@ const { tables } = require('..');
 
 module.exports = {
   up: async (knex) => {
-    await knex.schema.createTable(tables.gebruiker, (table) => {
+    await knex.schema.createTable(tables.user, (table) => {
       table.increments('idUser').primary();
       table.string('soort', 255).notNullable();
-
       table.string('gebruikersnaam', 255).notNullable();
       table.string('password_hash').notNullable(); 
       table.boolean('isActief').notNullable();
       table.jsonb('roles').notNullable();
       table.string('klantnummer').notNullable();
       table.string('leveranciernummer').notNullable();
+      table.integer('idBedrijf').unsigned().notNullable();
 
-      table.integer("idBedrijf").unsigned().notNullable();
-      //comment
-
-      table
-        .foreign('idBedrijf', "fk_user_bedrijf")
+      table.foreign('idBedrijf', "fk_user_bedrijf")
         .references(`${tables.bedrijf}.idBedrijf`)
         .onDelete('CASCADE');
     });
