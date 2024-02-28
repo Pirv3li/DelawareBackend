761377df4fabd6590aa50a3cbd15ccb149acc209
IIC004 <igorcasteleyn@gmail.com>
2024-02-28 21:16:29 +0100

    getKlantbyId en getLeverancierById

--- src/rest/klant.js
+++ src/rest/klant.js
@@ -1,15 +1,15 @@
-const KoaRouter = require('@koa/router'); 
-const validate = require('../core/validation');
-const userService = require('../service/users');
-const Joi = require('joi');
-const { requireAuthentication } = require('../core/auth'); 
+const KoaRouter = require("@koa/router");
+const validate = require("../core/validation");
+const userService = require("../service/users");
+const Joi = require("joi");
+const { requireAuthentication } = require("../core/auth");
 
 const login = async (ctx) => {
-  const { username, password } = ctx.request.body; 
-  const token = await userService.loginKlant(username, password); 
-  ctx.body = token; 
+  const { username, password } = ctx.request.body;
+  const token = await userService.loginKlant(username, password);
+  ctx.body = token;
 };
-login.validationScheme = { 
+login.validationScheme = {
   body: {
     username: Joi.string(),
     password: Joi.string(),
@@ -26,14 +26,38 @@ const getKlant = async (ctx) => {
       ctx.body = klant;
     } else {
       ctx.status = 404;
-      ctx.body = { message: 'Klant not found' };
+      ctx.body = { message: "Klant not found" };
     }
   } catch (error) {
     ctx.status = 500;
-    ctx.body = { message: 'Error fetching klant data' };
+    ctx.body = { message: "Error fetching klant data" };
   }
 };
 getKlant.validationScheme = null;
+
+const getKlantById = async (ctx) => {
+  try {
+    const { id } = ctx.params;
+    const klant = await userService.getKlantById(id);
+
+    if (klant) {
+      ctx.status = 200;
+      ctx.body = klant;
+    } else {
+      ctx.status = 404;
+      ctx.body = { message: "klant not found" };
+    }
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { message: "Error fetching klant data" };
+  }
+};
+
+getKlantById.validationScheme = {
+  params: {
+    id: Joi.number().required(),
+  },
+};
 
 /**
  * Install team routes in the given router.
@@ -42,11 +66,21 @@ getKlant.validationScheme = null;
  */
 module.exports = (router) => {
   const userRouter = new KoaRouter({
-    prefix: '/klant',
+    prefix: "/klant",
   });
   // public
-  userRouter.post('/login', validate(login.validationScheme), login);
-  userRouter.get('/', requireAuthentication, validate(getKlant.validationScheme), getKlant);
-
+  userRouter.post("/login", validate(login.validationScheme), login);
+  userRouter.get(
+    "/",
+    requireAuthentication,
+    validate(getKlant.validationScheme),
+    getKlant
+  );
+  userRouter.get(
+    "/:id",
+    requireAuthentication,
+    validate(getKlantById.validationScheme),
+    getKlantById
+  );
   router.use(userRouter.routes()).use(router.allowedMethods());
-}
+};
