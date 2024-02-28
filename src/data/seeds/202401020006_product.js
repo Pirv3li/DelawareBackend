ddb8e432df7909e49cb25ba33adffbe5aab7f56e
MohamedAzahaf <mohamed.azahaf@student.hogent.be>
2024-02-28 12:25:01 +0100

    migration product aangepast + seeds

--- src/data/seeds/202401020006_product.js
+++ src/data/seeds/202401020006_product.js
@@ -4,22 +4,28 @@ module.exports = {
   seed: async (knex) => {
     await knex(tables.product).delete(); 
 
-    await knex(tables.product).insert([{
+    await knex(tables.product).insert([
+      {
         idProduct: 1,
         idLeverancier: 1,
         naam: 'kaas',
         eenheidsprijs: 4,
         btwtarief: 6,
-        foto: 'https://media.nu.nl/m/un2xpm3ag029_wd854/zwitserse-kaas.jpg'
+        foto: 'https://media.nu.nl/m/un2xpm3ag029_wd854/zwitserse-kaas.jpg',
+        aantal: 10,
+        gewicht: 0.50,
+        beschrijving: 'Heerlijke Georgeaanse kaas'
       },
-
       {
         idProduct: 2,
         idLeverancier: 1,
         naam: 'water',
         eenheidsprijs: 1,
         btwtarief: 6,
-        foto: 'https://assets-willemwever.kro-ncrv.nl/public/styles/hero_small/public/water.jpeg?h=a8e2b6ba&itok=ckfRmCSB'
+        foto: 'https://assets-willemwever.kro-ncrv.nl/public/styles/hero_small/public/water.jpeg?h=a8e2b6ba&itok=ckfRmCSB',
+        aantal: 20,
+        gewicht: 1.00,
+        beschrijving: 'Verfrissend flessenwater uit Georgië'
       },
       {
         idProduct: 3,
@@ -27,7 +33,10 @@ module.exports = {
         naam: 'brood',
         eenheidsprijs: 3,
         btwtarief: 6,
-        foto: 'https://www.detrog.be/_userfiles/products/500x500/20220713091003ep0676-productfoto-hires-1.jpg'
+        foto: 'https://www.detrog.be/_userfiles/products/500x500/20220713091003ep0676-productfoto-hires-1.jpg',
+        aantal: 15,
+        gewicht: 0.75,
+        beschrijving: 'Versgebakken Yamanese brood'
       },
     ]);
   },
