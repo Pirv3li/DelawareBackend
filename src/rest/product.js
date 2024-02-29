8e3d015acb22ff1507858b890b2d17819b371c2c
Mohamed Alsagher <mohamed.alsagher@student.hogent.be>
2024-02-29 11:48:05 +0100

    product restlaag correcte versie

--- src/rest/product.js
+++ src/rest/product.js
@@ -2,7 +2,10 @@ const KoaRouter = require("@koa/router");
 const Joi = require("joi");
 const validate = require("../core/validation");
 const ServiceProducten = require("../service/product");
-const { requireAuthentication, makeRequireRole } = require("../core/auth");
+const {
+  requireAuthentication,
+  makeRequireRole
+} = require("../core/auth");
 const Role = require("../core/roles");
 
 const getProducten = async (ctx) => {
@@ -12,7 +15,9 @@ const getProducten = async (ctx) => {
     ctx.body = producten;
     ctx.status = 200;
   } catch (error) {
-    ctx.body = { message: "Error while fetching producten" };
+    ctx.body = {
+      message: "Error while fetching producten"
+    };
     ctx.status = 500;
   }
 };
@@ -25,7 +30,9 @@ getProducten.validationScheme = {};
 
 const createProducten = async (ctx) => {
   // try {
-  const { idLeverancier } = ctx.state.session;
+  const {
+    idLeverancier
+  } = ctx.state.session;
 
   const {
     foto,
@@ -71,28 +78,93 @@ createProducten.validationScheme = {
   },
 };
 
-const requireLeverancier = makeRequireRole(Role.LEVER);
+const PutProducten = async (ctx) => {
+      try {
+        const {
+          idLeverancier
+        } = ctx.state.session;
 
-/**
- * Install team routes in the given router.
- *
- * @param {KoaRouter} router - The Koa router.
- */
-module.exports = (router) => {
-  const userRouter = new KoaRouter({
-    prefix: "/producten",
-  });
-  // public
-  userRouter.get("/", validate(getProducten.validationScheme), getProducten);
-  userRouter.get("/:id", getProductByID);
-  //private
-  userRouter.post(
-    "/",
-    requireAuthentication,
-    requireLeverancier,
-    validate(createProducten.validationScheme),
-    createProducten
-  );
+        const leverancierUpdates = {};
+        if (ctx.request.body.foto) {
+          leverancierUpdates.foto = ctx.request.body.foto;
+        }
 
-  router.use(userRouter.routes()).use(router.allowedMethods());
-};
+        if (ctx.request.body.naam) {
+          leverancierUpdates.naam = ctx.request.body.naam;
+        }
+
+        if (ctx.request.body.eenheidsprijs) {
+          leverancierUpdates.eenheidsprijs = eenheidsprijs;
+        }
+
+        if (ctx.request.body.btwtarief) {
+          leverancierUpdates.btwtarief = ctx.request.body.btwtarief;
+        }
+
+        if (ctx.request.body.aantal) {
+          leverancierUpdates.aantal = ctx.request.body.aantal;
+        }
+
+        if (ctx.request.body.gewicht) {
+          leverancierUpdates.gewicht = ctx.request.body.gewicht;
+        }
+
+        if (ctx.request.body.beschrijving) {
+          leverancierUpdates.beschrijving = ctx.request.body.beschrijving;
+        }
+
+        const updatedProd = await ServiceProducten.PutProducten(idLeverancier, leverancierUpdates);
+
+        ctx.body = updatedProd;
+        ctx.status = 200;
+      } catch (error) {
+        if (ctx.status === 403) {
+          ctx.body = {
+            message: "Permission denied"
+          };
+        } else {
+          ctx.status = 500;
+          ctx.body = {
+            message: error
+          };
+        }
+      };
+      
+      PutProducten.validationScheme = {
+        body: {
+          foto: Joi.string().optional(),
+          naam: Joi.string().required(),
+          eenheidsprijs: Joi.number().positive().optional(),
+          btwtarief: Joi.number().positive().optional(),
+          aantal: Joi.number().integer().optional(),
+          gewicht: Joi.number().precision(2).positive().optional(),
+          beschrijving: Joi.string().max(255).optional(),
+        },
+      };
+
+
+      const requireLeverancier = makeRequireRole(Role.LEVER);
+
+      /**
+       * Install team routes in the given router.
+       *
+       * @param {KoaRouter} router - The Koa router.
+       */
+      module.exports = (router) => {
+        const userRouter = new KoaRouter({
+          prefix: "/producten",
+        });
+        // public
+        userRouter.get("/", validate(getProducten.validationScheme), getProducten);
+        userRouter.get("/:id", getProductByID);
+        //private
+        userRouter.post(
+          "/",
+          requireAuthentication,
+          requireLeverancier,
+          validate(createProducten.validationScheme),
+          createProducten
+        );
+
+        router.use(userRouter.routes()).use(router.allowedMethods());
+      };
